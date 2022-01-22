b92a845d25949c855e0a56c39a3ea5dfe50ccd27
Chu Nam Thắng <namthangmu123@gamil.com>
2022-01-22 16:59:57 +0700

    17:00 22/1/2022 - First commit

---
+++ app/src/main/java/com/thanguit/tiushop/application/MyApplication.java
@@ -0,0 +1,47 @@
+package com.thanguit.tiushop.application;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.facebook.imagepipeline.core.ImageTranscoderType;
+import com.facebook.imagepipeline.core.MemoryChunkType;
+
+public class MyApplication extends Application {
+    private static MyApplication instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (instance == null) {
+            instance = this;
+        }
+
+        Fresco.initialize(
+                this,
+                ImagePipelineConfig.newBuilder(this)
+                        .setMemoryChunkType(MemoryChunkType.BUFFER_MEMORY)
+                        .setImageTranscoderType(ImageTranscoderType.JAVA_TRANSCODER)
+                        .experiment().setNativeCodeDisabled(true)
+                        .build());
+    }
+
+    public static Resources getResource() {
+        return instance.getApplicationContext().getResources();
+    }
+
+    private boolean isNetworkConnected() { // Check Network Connected (true: Has network; false: No network)
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
+    }
+
+    public static boolean isNetwork() {
+        return instance.isNetworkConnected();
+    }
+}
