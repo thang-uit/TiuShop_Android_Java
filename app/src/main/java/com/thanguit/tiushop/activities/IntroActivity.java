88be91a92f64af099e85e4318937eb8c81197277
Chu Nam Thắng <namthangmu123@gamil.com>
2022-01-23 00:45:04 +0700

    00:44 23/1/2022 - Prepare for  something

--- app/src/main/java/com/thanguit/tiushop/activities/IntroActivity.java
+++ app/src/main/java/com/thanguit/tiushop/activities/IntroActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.thanguit.tiushop.R;
 
-public class MainActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
     }
 }
