9de47d44d79df58a1032065a875342ca191b2e79
Chu Nam Thắng <namthangmu123@gamil.com>
2022-01-24 00:50:11 +0700

    00:50 24/1/2022 - Theme

--- app/src/main/java/com/thanguit/tiushop/activities/IntroActivity.java
+++ app/src/main/java/com/thanguit/tiushop/activities/IntroActivity.java
@@ -3,6 +3,7 @@ package com.thanguit.tiushop.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.thanguit.tiushop.R;
 
@@ -11,6 +12,20 @@ public class IntroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            Window window = this.getWindow();
+//            // clear FLAG_TRANSLUCENT_STATUS flag:
+//            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//            // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//        }
+
+        // https://developer.android.com/training/system-ui/status#41
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_intro);
     }
 }
